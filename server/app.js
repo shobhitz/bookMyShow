cdb9b74fcfb5e66a24d847ea9ae692335273fe65
Shobhit <shobhitgoel1990@gmail.com>
2024-10-22 16:51:56 +0530

    added cors to server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express")
+const cors = require("cors");
 require("dotenv").config();
+
 const userRoute = require("./routes/userRoutes")
 const movieRoute = require("./routes/movieRoutes")
 
@@ -8,6 +10,7 @@ const dbConfig = require("./config/dbConfig")
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use("/api/user", userRoute)
 app.use("/api/movie", movieRoute)
 
